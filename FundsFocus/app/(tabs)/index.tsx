86d89a0727e2a34256dbfcfc93a60ddd269389d6
TiagoShigueo <tiago.yamasaki@gmail.com>
2024-03-27 15:56:06 -0300

    [ADD] Projeto base do Trabalho final FundsFocus

---
+++ FundsFocus/app/(tabs)/index.tsx
@@ -0,0 +1,29 @@
+import { StyleSheet } from 'react-native';
+
+import Home from '@/components/Home';
+import { Text, View } from '@/components/Themed';
+
+export default function TabOneScreen() {
+  return (
+    <View style={styles.container}>
+      <Home/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});
---
+++ FundsFocus/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from './Themed';
+
+export default function Home () {
+    return (
+        <>
+            <View>
+                <Text>Rodando tela Home</Text>
+            </View>
+        </>
+    )
+}
