f878e0667ada97065619cfed8462233d2dcda16e
TiagoShigueo <tiago.yamasaki@gmail.com>
2024-04-19 19:49:17 -0300

    [ADD] 2.Stocks-Price List of stocks

--- 2.Stocks-Price/src/app/(tabs)/index.tsx
+++ 2.Stocks-Price/src/app/(tabs)/index.tsx
@@ -1,12 +1,22 @@
-import { StyleSheet } from 'react-native';
-
-import { Text, View } from '@/src/components/Themed';
+import { StyleSheet, FlatList } from "react-native";
+import { Text, View } from "@/src/components/Themed";
+import { Stack } from "expo-router";
+import top5 from "@/assets/data/top5.json";
+import StockListItem from "@/src/components/StockListItem";
 
 export default function TabOneScreen() {
+  const stocks = Object.values(top5); /*Transformando Objeto em array*/
+
   return (
     <View style={styles.container}>
+      {/*Alterando o título da página*/}
+      <Stack.Screen options={{ title: "Stocks" }} />
 
-
+      <FlatList
+        data={stocks}
+        renderItem={({ item }) => <StockListItem stock={item} />}
+        contentContainerStyle={{ gap: 20, padding: 10 }} // Espaçamento interno da FlatList
+      />
     </View>
   );
 }
@@ -15,5 +25,4 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-
 });
---
+++ 2.Stocks-Price/src/components/StockListItem.tsx
@@ -0,0 +1,59 @@
+/* Mesmo Text que do React Native, porém importando do Themed altera com o Dark mode e Light mode */
+import { Text, View } from "./Themed";
+import { MonoText } from "./StyledText";
+import { StyleSheet } from "react-native";
+import Colors from "../constants/Colors";
+import { AntDesign } from "@expo/vector-icons";
+
+// Declarando que o name dentro do Stock é uma string
+type Stock = {
+  name: string;
+  symbol: string;
+  close: string;
+  percent_change: string;
+};
+
+//  Declarando que o Stock dentro de StockListItem é um Stock declarando anteriormente
+type StockListItem = {
+  stock: Stock;
+};
+
+export default function StockListItem({ stock }: StockListItem) {
+  const change = Number.parseFloat(stock.percent_change);
+
+  return (
+    <View style={styles.container}>
+      {/*Container da esquerda */}
+      <View style={{ flex: 1, gap: 5 }}>
+        {/*Flex: 1 joga o outro container filho para o outro lado */}
+        <Text style={styles.symbol}>
+          {stock.symbol} <AntDesign name="staro" size={18} color="gray" />
+        </Text>
+        <Text style={{ color: "gray" }}>{stock.name}</Text>
+      </View>
+
+      {/*Container da direita */}
+      <View style={{ alignItems: "flex-end" }}>
+        {/* Tem um componente StyledText que contém esse MonoText, mas só altera a fonte 
+        Convertendo para number a justando a quantidade de casas decimais com o toFixed */}
+        <MonoText>${Number.parseFloat(stock.close).toFixed(1)}</MonoText>
+        {/* Declarei change no começo da função, mas acho que daria para fazer igual do sotck.close */}
+        <MonoText style={{ color: change > 0 ? "green" : "red" }}>
+          {change > 0 ? "+" : ""}
+          {change.toFixed(1)}%
+        </MonoText>
+      </View>
+    </View>
+  ); // Return só retorna um componente, porém podemos colocar tudo dentro de uma View
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+  },
+  symbol: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: Colors.light.tint,
+  },
+});
