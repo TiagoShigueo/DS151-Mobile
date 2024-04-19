6baf045b3e85103f71817e0344e5522f642514b0
TiagoShigueo <tiago.yamasaki@gmail.com>
2024-04-19 20:25:03 -0300

    [ADD] 2.Stock-Price Stock details screen

---
+++ 2.Stocks-Price/src/app/[symbol].tsx
@@ -0,0 +1,28 @@
+// Para o parâmetro dinâmico, podemos utilizar ferramenta no nome do arquivo [symbol], mas podia ser [id]
+import { View, Text } from "@/src/components/Themed";
+import { Stack, useLocalSearchParams } from "expo-router";
+import top5 from "@/assets/data/top5.json";
+import StockListItem from "../components/StockListItem";
+
+const StockDetails = () => {
+  // Utiliza a função useLocalSerchParams para puxar o parâmetro passado no Link do arquivo StockListItem
+  const { symbol } = useLocalSearchParams();
+
+  const stock = top5[symbol];
+
+  if (!stock) {
+    return <Text>Stock with symbol {symbol} could not be found</Text>;
+  }
+
+  return (
+    <View style={{ padding: 10 }}>
+      <Stack.Screen
+        options={{ title: stock.symbol, headerBackTitleVisible: false }}
+      />
+      <StockListItem stock={stock} />
+    </View>
+  );
+};
+
+// Para aparecer, precisa sempre exportar
+export default StockDetails;
--- 2.Stocks-Price/src/components/StockListItem.tsx
+++ 2.Stocks-Price/src/components/StockListItem.tsx
@@ -1,9 +1,10 @@
 /* Mesmo Text que do React Native, porém importando do Themed altera com o Dark mode e Light mode */
 import { Text, View } from "./Themed";
 import { MonoText } from "./StyledText";
-import { StyleSheet } from "react-native";
+import { StyleSheet, Pressable } from "react-native";
 import Colors from "../constants/Colors";
 import { AntDesign } from "@expo/vector-icons";
+import { Link } from "expo-router";
 
 // Declarando que o name dentro do Stock é uma string
 type Stock = {
@@ -22,28 +23,32 @@ export default function StockListItem({ stock }: StockListItem) {
   const change = Number.parseFloat(stock.percent_change);
 
   return (
-    <View style={styles.container}>
-      {/*Container da esquerda */}
-      <View style={{ flex: 1, gap: 5 }}>
-        {/*Flex: 1 joga o outro container filho para o outro lado */}
-        <Text style={styles.symbol}>
-          {stock.symbol} <AntDesign name="staro" size={18} color="gray" />
-        </Text>
-        <Text style={{ color: "gray" }}>{stock.name}</Text>
-      </View>
+    // Link importado do expo-router joga para a página desejada Link href={"/stock"}
+    // Ou com parâmetro utiliza crase
+    <Link href={`/${stock.symbol}`} asChild>
+      <Pressable style={styles.container}>
+        {/*Container da esquerda */}
+        <View style={{ flex: 1, gap: 5 }}>
+          {/*Flex: 1 joga o outro container filho para o outro lado */}
+          <Text style={styles.symbol}>
+            {stock.symbol} <AntDesign name="staro" size={18} color="gray" />
+          </Text>
+          <Text style={{ color: "gray" }}>{stock.name}</Text>
+        </View>
 
-      {/*Container da direita */}
-      <View style={{ alignItems: "flex-end" }}>
-        {/* Tem um componente StyledText que contém esse MonoText, mas só altera a fonte 
+        {/*Container da direita */}
+        <View style={{ alignItems: "flex-end" }}>
+          {/* Tem um componente StyledText que contém esse MonoText, mas só altera a fonte 
         Convertendo para number a justando a quantidade de casas decimais com o toFixed */}
-        <MonoText>${Number.parseFloat(stock.close).toFixed(1)}</MonoText>
-        {/* Declarei change no começo da função, mas acho que daria para fazer igual do sotck.close */}
-        <MonoText style={{ color: change > 0 ? "green" : "red" }}>
-          {change > 0 ? "+" : ""}
-          {change.toFixed(1)}%
-        </MonoText>
-      </View>
-    </View>
+          <MonoText>${Number.parseFloat(stock.close).toFixed(1)}</MonoText>
+          {/* Declarei change no começo da função, mas acho que daria para fazer igual do sotck.close */}
+          <MonoText style={{ color: change > 0 ? "green" : "red" }}>
+            {change > 0 ? "+" : ""}
+            {change.toFixed(1)}%
+          </MonoText>
+        </View>
+      </Pressable>
+    </Link>
   ); // Return só retorna um componente, porém podemos colocar tudo dentro de uma View
 }
 
