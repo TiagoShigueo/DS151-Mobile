1ed85d8cb41d0455c83c6c76683c734f9273370b
TiagoShigueo <tiago.yamasaki@gmail.com>
2024-04-22 18:29:32 -0300

    [ADD] 2.Stocks-Price query stock details

--- 2.Stocks-Price/src/app/[symbol].tsx
+++ 2.Stocks-Price/src/app/[symbol].tsx
@@ -1,19 +1,37 @@
 // Para o parâmetro dinâmico, podemos utilizar ferramenta no nome do arquivo [symbol], mas podia ser [id]
 import { View, Text } from "@/src/components/Themed";
 import { Stack, useLocalSearchParams } from "expo-router";
-import top5 from "@/assets/data/top5.json";
 import StockListItem from "../components/StockListItem";
 import Graph from "../components/Graph";
+import { useQuery, gql } from "@apollo/client";
+import { ActivityIndicator } from "react-native";
+
+const query = gql`
+  query MyQuery($symbol: String) {
+    quote(symbol: $symbol) {
+      name
+      symbol
+      close
+      percent_change
+    }
+  }
+`;
 
 const StockDetails = () => {
   // Utiliza a função useLocalSerchParams para puxar o parâmetro passado no Link do arquivo StockListItem
   const { symbol } = useLocalSearchParams();
 
-  const stock = top5[symbol];
+  const { data, loading, error } = useQuery(query, { variables: { symbol } });
 
-  if (!stock) {
-    return <Text>Stock with symbol {symbol} could not be found</Text>;
+  if (loading) {
+    return <ActivityIndicator />;
   }
+
+  if (error) {
+    return <Text>Stocks with symbol {symbol} could not be found </Text>;
+  }
+
+  const stock = data.quote;
 
   return (
     <View style={{ padding: 10 }}>
