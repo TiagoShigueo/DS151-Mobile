4145dffc0914cb10e6f5fd2fff50438123c35003
TiagoShigueo <tiago.yamasaki@gmail.com>
2024-04-23 22:42:51 -0300

    [ADD] 3.StyleSheets exemplo StyleSheets em linha

--- 3.StyleSheets/src/App.js
+++ 3.StyleSheets/src/App.js
@@ -1,19 +1,10 @@
-import { StyleSheet, Text, View } from 'react-native';
-import Colors from './styles/Colors.ts';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container1}>
-      <Text style={Colors.verde}>Hello World verde</Text>
-      <Text style={Colors.vermelho}>Hello World vermelho</Text>
+    <View style={{ flex: 1, alignItems: 'center' }}>
+      <Text style={{ backgroundColor: 'green' }}>Hello World verde</Text>
+      <Text style={{ backgroundColor: 'red' }}>Hello World vermelho</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container1: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
