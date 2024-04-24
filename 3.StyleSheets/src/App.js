b6cc0107c3a901c11173de20c016784eecc50471
TiagoShigueo <tiago.yamasaki@gmail.com>
2024-04-23 23:39:35 -0300

    [ADD] 3.StyleSheets exemplo com StyleSheets no app.js

--- 3.StyleSheets/src/App.js
+++ 3.StyleSheets/src/App.js
@@ -1,10 +1,24 @@
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{ flex: 1, alignItems: 'center' }}>
-      <Text style={{ backgroundColor: 'green' }}>Hello World verde</Text>
-      <Text style={{ backgroundColor: 'red' }}>Hello World vermelho</Text>
+    <View style={ style.container}>
+      <Text style={ style.green }>Hello World verde</Text>
+      <Text style={ style.red }>Hello World vermelho</Text>
     </View>
   );
 }
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  green: {
+    backgroundColor: 'green',
+  },
+  red: {
+    backgroundColor: 'red',
+  },
+});
