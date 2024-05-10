3df71a0e20e1e02e12be201ae6e248f13e33a0af
TiagoShigueo <tiago.yamasaki@gmail.com>
2024-05-10 15:32:43 -0300

    [ADD] Projeto base Firebase com configuração básica

---
+++ 5.Firebase/src/services/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD6DkVFFhP01yZPQLAASiLqp6oCdyP3g9A",
+  authDomain: "fir-b9d52.firebaseapp.com",
+  projectId: "fir-b9d52",
+  storageBucket: "fir-b9d52.appspot.com",
+  messagingSenderId: "356926613214",
+  appId: "1:356926613214:web:3f43124c977813c117844f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
