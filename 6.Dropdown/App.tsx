fa34480783beddabaa5c4edc1b5eca9d82070698
TiagoShigueo <tiago.yamasaki@gmail.com>
2024-05-17 15:34:45 -0300

    [ADD] Dropdown com lista dos fundos obtidos via CSV

--- 6.Dropdown/App.tsx
+++ 6.Dropdown/App.tsx
@@ -1,18 +1,172 @@
-import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
+{
+  /*Opção Dropdown element*/
+}
+import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
-import DropDownPicker from "react-native-dropdown-picker";
+import { Dropdown } from "react-native-element-dropdown";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import { readRemoteFile } from "react-native-csv";
+// const data = [
+//   { label: "Item 1", value: "1" },
+//   { label: "Item 2", value: "2" },
+//   { label: "Item 3", value: "3" },
+//   { label: "Item 4", value: "4" },
+//   { label: "Item 5", value: "5" },
+//   { label: "Item 6", value: "6" },
+//   { label: "Item 7", value: "7" },
+//   { label: "Item 8", value: "8" },
+// ];
 
+const DropdownComponent = () => {
+  const [value, setValue] = useState<string | null>(null);
+
+  // const [value, setValue] = useState(null);
+  const [isFocus, setIsFocus] = useState(false);
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    readRemoteFile("./fundosListadosEdited.csv", {
+      complete: (results: any) => {
+        const csvDataArray = results.data;
+        const convertedData = csvDataArray.map(
+          (item: { [x: string]: any }) => ({
+            label: item["Codigo"],
+            value: item["Codigo"],
+          })
+        );
+        console.log("convertedData: ", convertedData);
+        setItems(convertedData);
+      },
+
+      header: true,
+    });
+  }, []);
+  // const renderLabel = () => {
+  //   if (value || isFocus) {
+  //     return (
+  //       <Text style={[styles.label, isFocus && { color: "blue" }]}>
+  //         Dropdown label
+  //       </Text>
+  //     );
+  //   }
+  //   return null;
+  // };
+
+  return (
+    <View style={styles.container}>
+      {/* {renderLabel()} */}
+      <Dropdown
+        style={[styles.dropdown, isFocus && { borderColor: "blue" }]}
+        // placeholderStyle={styles.placeholderStyle}
+        // selectedTextStyle={styles.selectedTextStyle}
+        // inputSearchStyle={styles.inputSearchStyle}
+        // iconStyle={styles.iconStyle}
+        data={items}
+        search
+        // maxHeight={300}
+        labelField="label"
+        valueField="value"
+        placeholder={!isFocus ? "Select item" : "Selecione o fundo"}
+        searchPlaceholder="Search..."
+        value={value}
+        onFocus={() => setIsFocus(true)}
+        onBlur={() => setIsFocus(false)}
+        onChange={(item) => {
+          setValue(item.value);
+          setIsFocus(false);
+        }}
+        // renderLeftIcon={() => (
+        //   <AntDesign
+        //     style={styles.icon}
+        //     color={isFocus ? "blue" : "black"}
+        //     name="Safety"
+        //     size={20}
+        //   />
+        // )}
+      />
+    </View>
+  );
+};
+
+export default DropdownComponent;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    padding: 16,
+  },
+  dropdown: {
+    height: 50,
+    borderColor: "gray",
+    borderWidth: 0.5,
+    borderRadius: 8,
+    paddingHorizontal: 8,
+  },
+  icon: {
+    marginRight: 5,
+  },
+  label: {
+    position: "absolute",
+    backgroundColor: "white",
+    left: 22,
+    top: 8,
+    zIndex: 999,
+    paddingHorizontal: 8,
+    fontSize: 14,
+  },
+  placeholderStyle: {
+    fontSize: 16,
+  },
+  selectedTextStyle: {
+    fontSize: 16,
+  },
+  iconStyle: {
+    width: 20,
+    height: 20,
+  },
+  inputSearchStyle: {
+    height: 40,
+    fontSize: 16,
+  },
+});
+
+{
+  /* Opção Dropdown Picker
+import React, { useEffect, useState } from "react";
+import { View, Text } from "react-native";
+import DropDownPicker from "react-native-dropdown-picker";
+import { readRemoteFile } from "react-native-csv";
+import { Dropdown } from "react-native-element-dropdown";
+interface Fruit {
+  label: string;
+  value: string;
+}
+const initialItems: Fruit[] = [];
 export default function App() {
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState(null);
-  const [items, setItems] = useState([
-    { label: "Apple", value: "apple" },
-    { label: "Banana", value: "banana" },
-    { label: "Pear", value: "pear" },
-  ]);
+  const [items, setItems] = useState(initialItems);
+  const [csvData, setCsvData] = useState([]);
+  useEffect(() => {
+    readRemoteFile("./fundosListadosEdited.csv", {
+      complete: (results: any) => {
+        const csvDataArray = results.data;
+        const convertedData = csvDataArray.map(
+          (item: { [x: string]: any }) => ({
+            label: item["Codigo"],
+            value: item["Codigo"],
+          })
+        );
+        console.log("convertedData: ", convertedData);
+        setItems(convertedData);
+      },
+
+      header: true,
+    });
+  }, []);
+
   return (
-    <View style={styles.container}>
+    <View style={{ flex: 1 }}>
+      <Text>Opção Dropdown Picker</Text>
       <View
         style={{
           flex: 1,
@@ -44,12 +198,5 @@ export default function App() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+*/
+}
--- 6.Dropdown/README.md
+++ 6.Dropdown/README.md
@@ -1,3 +1,8 @@
 # Instalações
 
-`npm i react-native-dropdown-picker`
+`npm i react-native-dropdown-picker` Acho q não vou mais usar esse
+`npm install react-native-fs csv-parse` Acho q não deu certo esse csv-parse
+`npm install papaparse` Acho q não deu certo esse csv-parse
+`npm i --save-dev @types/papaparse`Acho q não deu certo esse csv-parse
+`npm install react-native-csv`
+`npm i react-native-element-dropdown`
